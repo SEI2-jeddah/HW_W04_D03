918d59c8d24a74821796f2ba61d6a271d0ff72bf
AnasShar <anas10895@gmail.com>
2019-10-23 06:32:14 +0300

    pic galary

---
+++ main.js
@@ -0,0 +1,212 @@
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/400x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/300x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/200x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/450x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/250x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/350x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+
+
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/550x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/410x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/345x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    console.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/330x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    cosole.log(err)
+})
+axios({
+    method:"get",
+    url: "https://source.unsplash.com/random/230x350"
+    
+
+})
+.then((result) => {
+image = result.request.responseURL
+console.log(result)
+$('body').append(`<img  class ="image" src =${image}/>`)
+
+$('.b').click(function() {
+    location.reload();
+});
+
+} ) 
+.catch(err =>{
+    cosole.log(err)
+})
